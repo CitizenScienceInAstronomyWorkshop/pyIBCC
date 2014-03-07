02bb4dc0eaebef9d6a072a4f6f10ece242b369b6
edwinrobots <edwin@robots.ox.ac.uk>
2014-03-07 04:40:50 +0000

    Reimplementation of IBCC in Python by Edwin. Needs testing on a larger dataset and comparing with matlab implementation

---
+++ python_edwin/config/my_project.py
@@ -0,0 +1,9 @@
+print 'Configuring IBCC'
+
+scores = np.array([3, 4])
+nScores = len(scores)
+nClasses = 2
+inputFile =   './data/input.csv'
+goldFile =    './data/gold.csv'
+outputFile =  './output/output.csv'
+confMatFile = './output/confMat.csv'
