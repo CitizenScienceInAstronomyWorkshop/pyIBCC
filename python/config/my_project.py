ac89929a7855476d1e6d5e2a87953424ff0ca2fc
Kyle Willett <willettk@gmail.com>
2014-03-23 23:22:37 -0500

    Run on first 500 classifications in GZ CANDELS, Task 01

--- python/config/my_project.py
+++ python/config/my_project.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 
-''' Configuration file for pyIBCC. Currently set for Galaxy Zoo (which version?) data. '''
+''' Configuration file for pyIBCC. Currently set for Galaxy Zoo CANDELS data. '''
 
 __author__ = "Kyle Willett"
 
 print 'Running my_project.py --- configuring IBCC'
 
-scores = np.array([3, 4])
+scores = np.array([0,1,2])
 nScores = len(scores)
-nClasses = 2
-inputFile =   './data/input.csv'
+nClasses = 3
+inputFile =   './data/gz_candels_input_1000.csv'
 goldFile =    './data/gold.csv'
 outputFile =  './output/output.csv'
 confMatFile = './output/confMat.csv'
 
-nu0 = np.array([50,50])
-alpha0 = np.array([[2, 1], 
-                   [1, 2]])
+nu0 = np.array([25,25,50])
+alpha0 = np.array([[2, 1, 1], [1, 2, 1], [1, 1, 2]])
 
 lang = 'en-us'
