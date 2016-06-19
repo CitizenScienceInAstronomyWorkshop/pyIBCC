b3c5e1eeaf99438b795cd48ce6028d9401b25fdd
edwinrobots <edwin.simpson@gmail.com>
2016-06-19 22:22:08 +0200

    tidy up lda code for tutorial

--- python/tutorial/lda_incomplete.py
+++ python/tutorial/lda_incomplete.py
@@ -99,31 +99,30 @@ def _update_doc_distribution(X, exp_topic_word_distr, doc_topic_prior,
             ids = np.nonzero(X[idx_d, :])[0]
             cnts = X[idx_d, ids]
 
+        # doc_topic_d has size K, i.e. no. topics
         doc_topic_d = doc_topic_distr[idx_d, :]
         # The next one is a copy, since the inner loop overwrites it.
         exp_doc_topic_d = exp_doc_topic[idx_d, :].copy()
+        #exp_topic_word_d has size K x W, i.e. no. topics x no. words
         exp_topic_word_d = exp_topic_word_distr[:, ids]
 
         # Iterate between `doc_topic_d` and `norm_phi` until convergence
         for _ in xrange(0, max_iters):
-            last_d = doc_topic_d
+            last_d = doc_topic_d # 1 x K 
 
-            # The optimal phi_{dwk} is proportional to
-            # exp(E[log(theta_{dk})]) * exp(E[log(beta_{dw})]).
-            
-            #normalisation
+            #normalisation: has size 1 x W 
             norm_phi = np.dot(exp_doc_topic_d, exp_topic_word_d) + EPS 
-            phi = exp_doc_topic_d[:, np.newaxis] * exp_topic_word_d / norm_phi[np.newaxis, :] # size: K x W, i.e. no. topics x no. words
-            # cnts has size 1 x W. Multiply the counts by the word likelihoods, and sum across all words:
+            # phi has size K x W, i.e. no. topics x no. words
+            phi = exp_doc_topic_d[:, np.newaxis] * exp_topic_word_d / norm_phi[np.newaxis, :] 
             
+            doc_topic_params = np.sum(phi, axis=1) + doc_topic_prior 
             
-            doc_topic_params = ... + doc_topic_prior
-
-            exp_doc_topic_d = psi(doc_topic_params) - psi( np.sum(doc_topic_params) )
+            doc_topic_d = psi(doc_topic_params) - psi( np.sum(doc_topic_params) )
+            exp_doc_topic_d = np.exp(doc_topic_d)
             
             if mean_change(last_d, doc_topic_d) < mean_change_tol:
                 break
-        doc_topic_distr[idx_d, :] = doc_topic_d
+        doc_topic_distr[idx_d, :] = doc_topic_d 
 
         # Contribution of document d to the expected sufficient
         # statistics for the M step.
