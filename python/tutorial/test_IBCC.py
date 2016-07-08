3a91d53cb633eb936181b3d09e393693fb05b3e8
Edwin Simpson <simpson@ukp.informatik.tu-darmstadt.de>
2016-07-08 10:42:35 +0200

    add tutorial code

---
+++ python/tutorial/test_IBCC.py
@@ -0,0 +1,10 @@
+'''
+Created on 13 Jun 2016
+
+@author: simpson
+
+A script to load and test pyIBCC with an example dataset. Allows users to change
+priors and see their effect, and visualise the worker reliability distributions.
+
+'''
+
